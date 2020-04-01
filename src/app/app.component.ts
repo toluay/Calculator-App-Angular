974bdaea73e5f80de17194077fe696db5c8e008f
Tolu Ayantunde <ayantundetolu@gmail.com>
2020-04-01 20:24:59 +0100

    test deployment with additin and subtraction application will later change to the newspaper app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'test-app';
+  title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+  add() {
+      this.result = this.num1 + this.num2;
+  }
+  substract() {
+      this.result = this.num1 - this.num2;
+  }
+  multiply() {
+      this.result = this.num1 * this.num2;
+  }
+  divide() {
+      this.result = this.num1 % this.num2;
+  }
 }
